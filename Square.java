02527defa5148502aa18e364318614c6cee21a94
abGray <a01611120@itesm.mx>
2020-07-11 15:12:03 -0500

    Revert "intento sabaDO"
    This reverts commit b31f06ebd663f9c75f5c4a479e314906bb04696b.

--- Square.java
+++ Square.java
@@ -25,8 +25,15 @@ public class Square {
     public boolean getTaken() {
         return taken;
     }
-    public boolean setTaken() {
-        return taken;
+    public void setTaken(boolean taken) {
+       this.taken = taken;
+    }
+    
+    public String toString() {
+        if(taken)
+            return "X";
+        else
+            return ".";
     }
 
 }
